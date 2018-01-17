8889dafa02c6ef88a644f2c4c0941d06b8df6990
Thinh Nguyen <thinhnp@ymese.com>
2018-01-17 12:03:54 +0700

    nit

--- wp-react-popup/__tests__/boilerplate_test.js
+++ wp-react-popup/__tests__/boilerplate_test.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-
-
-// import { expect } from 'chai';
 import Link from '../dist-modules';
 
 describe('Boilerplate', function () {
   it('should render correctly', function () {
     // TODO: test something now
     const component = renderer
-      .create(
-        <Link page="https://ymese.com">Ymese 123</Link>
-      )
+      .create(<Link page="https://ymese.com">Ymese 123</Link>)
       .toJSON();
-
     expect(component).toMatchSnapshot();
   });
 });
