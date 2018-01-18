4a18c349ab7315521e945f1181107b50c5d88e04
Thinh Nguyen <thinhnp@ymese.com>
2018-01-18 09:29:32 +0700

    Create tab example

--- wp-react-popup/examples/app/src/App.js
+++ wp-react-popup/examples/app/src/App.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
+import Tab from 'wp-react-toolkits';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.tabs = [{
+      name: 'This is tab1',
+      panel: (
+        <div><p>Hello tab1</p></div>
+      )
+    }, {
+      name: 'This is tab2',
+      panel: (
+        <div><p>Hello tab2</p></div>
+      )
+    }];
+  }
   render() {
     return (
       <div className="App">
@@ -13,6 +30,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>This is a tab</div>
+        <div>
+          <Tab tabs={this.tabs}/>
+        </div>
       </div>
     );
   }
